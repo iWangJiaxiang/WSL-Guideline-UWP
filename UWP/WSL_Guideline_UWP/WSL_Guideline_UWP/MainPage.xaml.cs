5b728bacd74a38f51e2858bff44846f0ebd5a2b4
嘉祥 王 <imfelix@live.com>
2018-03-14 23:27:50 +0800

    可行性分析完成

--- UWP/WSL_Guideline_UWP/WSL_Guideline_UWP/MainPage.xaml.cs
+++ UWP/WSL_Guideline_UWP/WSL_Guideline_UWP/MainPage.xaml.cs
@@ -12,19 +12,52 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+using Windows.UI.Xaml;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using WSL_Guideline_UWP.Models;
+using System.Collections.ObjectModel;
 
 namespace WSL_Guideline_UWP
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        //MVVM绑定文章
+        ObservableCollection<Article> Articles = new ObservableCollection<Article>();
+
         public MainPage()
         {
             this.InitializeComponent();
+            string localpic = ApplicationData.Current.LocalFolder.Path + "\\2.png";
+            #region 给List赋值
+            Articles.Add(new Article()
+            {
+                Name = "Article1",
+                Content = @"# WSL 使用指南
+
+### 01 WSL入门
+
+#### 什么是WSL
+
+![test](/Assets/StoreLogo.png)
+
+&emsp;&emsp; WSL是“Windows Subsystem for Linux”的缩写，顾名思义，WSL就是Windows系统的Linux子系统，其作为Windows组件搭载在Windows10周年更新（1607）后的Windows系统中。 "
+            });
+            Articles[0].Content = Articles[0].Content.Replace("/Assets/StoreLogo.png", localpic);
+            Articles.Add(new Article()
+            {
+                Name = "Article2",
+                Content = Articles[0].Content
+            });
+            Articles.Add(new Article()
+            {
+                Name = "Article2",
+                Content = Articles[0].Content
+            });
+            #endregion
         }
+
     }
 }
---
+++ UWP/WSL_Guideline_UWP/WSL_Guideline_UWP/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+namespace WSL_Guideline_UWP.Models
+{
+    public class Article : ObservableObject
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
