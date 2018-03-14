9d05358123277c8aca0430982ff9151701302c23
嘉祥 王 <imfelix@live.com>
2018-03-15 00:14:14 +0800

    创建单元测试项目

---
+++ UWP/WSL_Guideline_UWP/UnitTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSL_Guideline_UWP;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
