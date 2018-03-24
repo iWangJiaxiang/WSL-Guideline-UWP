619e547e29cde44944e75165f9dfe042fe59631d
嘉祥 王 <imfelix@live.com>
2018-03-25 00:22:18 +0800

    完成汉堡菜单框架

--- UWP/WSL_Guideline_UWP/WSL_Guideline_UWP/MainPage.xaml.cs
+++ UWP/WSL_Guideline_UWP/WSL_Guideline_UWP/MainPage.xaml.cs
@@ -12,10 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml;
 using Windows.Storage;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 using System.Threading.Tasks;
 using WSL_Guideline_UWP.Models;
 using System.Collections.ObjectModel;
@@ -23,6 +20,7 @@ using WSL_Guideline_UWP.Views;
 using Windows.ApplicationModel.Core;
 using Windows.UI.ViewManagement;
 using Windows.UI.Core;
+using Windows.UI;
 
 namespace WSL_Guideline_UWP
 {
@@ -33,7 +31,8 @@ namespace WSL_Guideline_UWP
             this.InitializeComponent();
             //初始化TitleBar
             InitializeTitleBar();
-            contentFrame.Navigate(typeof(ArticleMasterDetailView));
+            UpdateAppTitleVisibility();
+            ContentFrame.Navigate(typeof(ArticleMasterDetailView));
         }
 
         #region 自定义TitleBar
@@ -42,34 +41,75 @@ namespace WSL_Guideline_UWP
         {
             var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
-     
+
+            //remove the solid-colored backgrounds behind the caption controls and system back button
+            var viewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            viewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            viewTitleBar.ButtonForegroundColor = (Color)Resources["SystemBaseHighColor"];
+
             //TitleBar尺寸改变事件
             coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
             //TitleBar可见性改变事件
             coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
-            Window.Current.SetTitleBar(MyTitleBar);
+
+            Window.Current.SetTitleBar(AppTitleBar);
+
         }
+
         private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
         {
             UpdateTitleBarLayout(sender);
         }
+
         private void UpdateTitleBarLayout(CoreApplicationViewTitleBar sender)
         {
-            MyTitleBar.Height = sender.Height;
-            TitleTB.Margin = new Thickness(sender.SystemOverlayLeftInset+10, 0, 0, 0);
+            AppTitleBar.Height = sender.Height;
+            AppTitle.Margin = new Thickness(sender.SystemOverlayLeftInset + 10, 0, 0, 0);
+            UpdateNavView(sender.Height);
         }
+
         private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
         {
             if (sender.IsVisible)
             {
-                MyTitleBar.Visibility = Visibility.Visible;
+                AppTitleBar.Visibility = Visibility.Visible;
+                UpdateNavView(sender.Height);
             }
             else
             {
-                MyTitleBar.Visibility = Visibility.Collapsed;
+                AppTitleBar.Visibility = Visibility.Collapsed;
+                UpdateNavView(0);
             }
         }
         #endregion
 
+        /// 自定义页面内容上边距
+        private void UpdateNavView(double appTitleBarHeight)
+        {
+            var currentMarginTop = MenuButton.Height +appTitleBarHeight;
+            MenuListView.Margin = new Thickness(0, currentMarginTop, 0, 0);
+            ContentFrame.Padding = new Thickness(0, currentMarginTop, 0, 0);
+
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavView.IsPaneOpen = !NavView.IsPaneOpen;
+            UpdateAppTitleVisibility();
+        }
+
+        private void UpdateAppTitleVisibility()
+        {
+            if (NavView.IsPaneOpen)
+                AppTitle.Visibility = Visibility.Visible;
+            else
+                AppTitle.Visibility = Visibility.Collapsed;
+        }
+
+        private void MenuListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
     }
 }
